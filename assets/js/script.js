6051b91c23a11d19aebcad5da00fc4cdcd9b666a
Pietro907 <pietrop.puri@gmail.com>
2023-09-14 11:55:37 +0200

    add script descrition

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+/* 
+
+Descrizione:
+Iniziamo a lavorare alla nostra replica della nota app di messaggistica. L'esercitazione sará divisa in piú giornate, oggi iniziamo a lavorare alla prima milestone che vi
+riporto di seguito:
+
+Milestone 1
+Replica della grafica con la possibilità di avere messaggi scritti dall'utente (verdi) e dall'interlocutore (bianco) assegnando due classi CSS diverse
+Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
+
+
+Consigli:
+lavorate ad una task per volta, finita la prima passate alla seconda
+ragionate con calma sugli strumenti necessari per svolgere la seconda task,
+leggete le pagine della documentazione che vi ho indicato oggi in classe.
+
+*/
