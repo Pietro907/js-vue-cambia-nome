cf3b248930382e94c0a2801e62740b668712cc09
Pietro907 <pietrop.puri@gmail.com>
2023-09-18 17:30:55 +0200

    add

--- assets/js/script.js
+++ assets/js/script.js
@@ -218,7 +218,7 @@ createApp({
 
         
 
-        document.querySelector('.fa')
+        
         inviaMess(){
             if (newMessage.text != '') {
                 const threadEl = document.createElement('div');
